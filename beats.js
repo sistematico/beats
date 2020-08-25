0814a5b6b2f7964779f4b20f2db0123813d4f03e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-25 16:03:01 -0400

    Autocommit por GitHub: alterações: add 'beats.js'

--- beats.js
+++ beats.js
@@ -1,10 +1,10 @@
 const beats = _ => {
     //((UTC+1minutes * 60) + (UTC+1hours * 3600)) / 86.4
+    //((UTC+1minutes * 60) + (UTC+1hours * 3600)) / 86.4
     let d = new Date()
     let h = d.getUTCHours()
     let m = d.getUTCMinutes()
-    let s = d.getUTCSeconds()
-    let beats = (s + ((m * 60) + (h * 3600))) / 86.4;
+    let beats = ((m * 60) + (h * 3600)) / 86.4;
     document.getElementById("beats").innerHTML = Math.round((beats + Number.EPSILON) * 100) / 100;
 }
 
